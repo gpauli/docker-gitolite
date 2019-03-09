2c1ef09e1bcc31424757fb7028b926a6b021e71d
Gerd Pauli <gp@high-consulting.de>
2019-03-09 23:50:01 +0100

    added timezone settings

--- build.sh
+++ build.sh
@@ -7,7 +7,10 @@ mytag="docker.high-con.de/gitolite:3.9.2"
 
 echo "FROM $mybase" > Dockerfile
 echo "MAINTAINER \"Gerd Pauli <gp@high-consulting.de>\"" >> Dockerfile
-echo "RUN set -x && apk add --no-cache gitolite openssh && passwd -u git" >> Dockerfile
+echo "RUN set -x && apk add --no-cache gitolite openssh tzdata && passwd -u git" >> Dockerfile
+echo "RUN cp /usr/share/zoneinfo/Europe/Berlin /etc/localtime" >> Dockerfile
+echo "RUN echo \"Europe/Berlin\" >  /etc/timezone" >> Dockerfile
+echo "RUN apk del tzdata" >> Dockerfile
 echo "VOLUME [/etc/ssh/keys]" >> Dockerfile
 echo "VOLUME [/var/lib/git]" >> Dockerfile
 echo "COPY docker-entrypoint.sh /" >> Dockerfile
